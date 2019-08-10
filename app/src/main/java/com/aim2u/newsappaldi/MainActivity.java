06c9b1faa8d020fe10604b45e4b75877d5fd0068
imaldi <imaldi@users.noreply.github.com>
2019-08-10 15:25:40 +0700

    RecyclerView Siappp

--- app/src/main/java/com/aim2u/newsappaldi/MainActivity.java
+++ app/src/main/java/com/aim2u/newsappaldi/MainActivity.java
@@ -1,14 +1,78 @@
 package com.aim2u.newsappaldi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.aim2u.newsappaldi.api.ApiClient;
+import com.aim2u.newsappaldi.api.ApiInterface;
+import com.aim2u.newsappaldi.models.Article;
+import com.aim2u.newsappaldi.models.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String API_KEY = "2ebd068ccfbb4331914b7f62638837ac";
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<Article> mArticles = new ArrayList<>();
+    private Adapter mAdapter;
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recView);
+        mLayoutManager = new LinearLayoutManager(MainActivity.this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setNestedScrollingEnabled(false);
+
+        LoadJson();
+    }
+
+    public void LoadJson(){
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+
+        Call<News> call;
+        call = apiInterface.getNews(country,API_KEY);
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if(response.isSuccessful() && response.body().getArticle() != null){
+                    if(!mArticles.isEmpty()){
+                        mArticles.clear();
+                    }
+
+                    mArticles = response.body().getArticle();
+                    mAdapter = new Adapter(mArticles, MainActivity.this);
+                    mRecyclerView.setAdapter(mAdapter);
+                    mAdapter.notifyDataSetChanged();
+
+                } else {
+                    Toast.makeText(MainActivity.this,"No Result!",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/aim2u/newsappaldi/api/ApiInterface.java
@@ -0,0 +1,15 @@
+package com.aim2u.newsappaldi.api;
+
+import com.aim2u.newsappaldi.models.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("top-headlines")
+    Call<News> getNews(
+        @Query("country") String country,
+        @Query("apiKey") String apiKey
+    );
+}

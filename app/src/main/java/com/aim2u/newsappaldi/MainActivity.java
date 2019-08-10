e1994a2b6a5db68922e45d121c06583c5ddfb242
imaldi <imaldi@users.noreply.github.com>
2019-08-10 11:43:55 +0700

    Commit pertama

---
+++ app/src/main/java/com/aim2u/newsappaldi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aim2u.newsappaldi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
